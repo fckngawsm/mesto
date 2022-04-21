a689d62e62bb6bacfb53a57e563bff58c4d0cace
Kirill <forworkskirill@gmail.com>
2022-04-21 21:44:32 +0500

    mesto

--- script.js
+++ script.js
@@ -2,28 +2,31 @@ let popup = document.querySelector ('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = popup.querySelector ('.popup__close');
 let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__type-name');
-let jobInput = formElement.querySelector('.popup__type-status');
-let buttonChange = popup.querySelector('.popup__submit-btn');
+let nameInput = formElement.querySelector('.popup__text_type-name');
+let jobInput = formElement.querySelector('.popup__text_type-status');
 let profileName = document.querySelector ('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
-function popupOpenToggle(){
-    popup.classList.toggle('popup__is-opened');
+function openPopupAdd(){
+    popup.classList.add('popup_is-opened');
 }
 
-openPopupButton.addEventListener('click', popupOpenToggle);
-closePopupButton.addEventListener('click', popupOpenToggle);
-buttonChange.addEventListener('click', popupOpenToggle);
+function closePopupRemove(){
+    popup.classList.remove('popup_is-opened')
+}
+closePopupButton.addEventListener('click', closePopupRemove);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    nameInput.textContent = nameInput.value;
-    jobInput.textContent = jobInput.value;
     profileName.textContent = nameInput.value;
     profileStatus.textContent = jobInput.value;
+    closePopupRemove()
 }
+
 formElement.addEventListener('submit', formSubmitHandler);
+openPopupButton.addEventListener('click', openPopupAdd);
+
+
 
 
 
