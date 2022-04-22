a5b808357da6122b502da143570bc2421a21e0c0
Kirill <forworkskirill@gmail.com>
2022-04-22 19:27:04 +0500

    mesto

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,15 @@ let popup = document.querySelector ('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = popup.querySelector ('.popup__close');
 let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__text_type-name');
-let jobInput = formElement.querySelector('.popup__text_type-status');
+let nameInput = formElement.querySelector('.popup__text_type_name');
+let jobInput = formElement.querySelector('.popup__text_type_status');
 let profileName = document.querySelector ('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
 function openPopupAdd(){
     popup.classList.add('popup_is-opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileStatus.textContent;
 }
 
 function closePopupRemove(){
