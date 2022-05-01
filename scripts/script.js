2c04f2f97b821025dac2a8e41d684730bd9491f6
Kirill <forworkskirill@gmail.com>
2022-05-01 20:02:13 +0500

    half work

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,24 @@
-let popup = document.querySelector ('.popup');
-let popupAdd = document.querySelector('.popup-add')
-let openPopupButton = document.querySelector('.profile__edit-button');
-let openPopupAddButton = document.querySelector('.profile__add-button')
-let closePopupButton = popup.querySelector ('.popup__close');
-let closePopupAddButton = popupAdd.querySelector ('.popup__close');
-let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__text_type_name');
-let jobInput = formElement.querySelector('.popup__text_type_status');
-let profileName = document.querySelector ('.profile__name');
-let profileStatus = document.querySelector('.profile__status');
-let elements = document.querySelector('.elements');
-let formPopupAdd = document.querySelector('.popup-add__form');
-let inputTitle = document.getElementById('title-input');
-let inputSource = document.getElementById('source-input');
+// переменные
+const popup = document.querySelector ('.popup');
+const popupAdd = document.querySelector('.popup-add')
+const openPopupButton = document.querySelector('.profile__edit-button');
+const openPopupAddButton = document.querySelector('.profile__add-button')
+const closePopupButton = popup.querySelector ('.popup__close');
+const closePopupAddButton = popupAdd.querySelector ('.popup__close');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__text_type_name');
+const jobInput = formElement.querySelector('.popup__text_type_status');
+const profileName = document.querySelector ('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+const elements = document.querySelector('.elements');
+const formPopupAdd = document.querySelector('.popup-add__form');
+const inputTitle = document.getElementById('title-input');
+const inputSource = document.getElementById('source-input');
 
+// tempalte
+const elementTemplate = document.querySelector('#template-element').content.querySelector('.element')
 
+// функции для открытия и закрытия "попапа"
 function openPopupAdd(){
     popup.classList.add('popup_is-opened');
     nameInput.value = profileName.textContent;
@@ -49,6 +53,15 @@ const handlerSubmitAddSourceForm = (evt) => {
     closePopupAddPhoto()
 }
 
+// функции для лайков и удаления карточек
+
+const handleLikeImages = (evt) => {
+    evt.target.closest('.element__like').classList.toggle('element__like_active');
+}
+
+const handleDeleteCard = (evt) => {
+    evt.target.closest('.element').remove();
+}
 
 // Работа с добавлением элементов
 
@@ -85,37 +98,41 @@ const initialCards = [
     },
 ];
 
-const addElement = (initialCards) => {
-    elements.insertAdjacentHTML('afterbegin',
-    `
-                <div class="element">
-                    <img class="element__image"
-                        src="${initialCards.link}"
-                        alt="${initialCards.alt}">
-                    <div class="element__info">
-                        <h2 class="element__title">
-                            ${initialCards.name}
-                        </h2>
-                        <button class="element__like" type="button">
-                        </button>
-                  </div>
-                </div>
-    `);
+generateInititalItem = (initialItem) => {
+    const newInitialItem = elementTemplate.cloneNode(true);
+
+    const titleInitialCards = newInitialItem.querySelector('.element__title');
+    const imageInitialCards = newInitialItem.querySelector('.element__image');
+
+    imageInitialCards.src = initialItem.link;
+    titleInitialCards.textContent = initialItem.name;
+
+    const likeImage = newInitialItem.querySelector('.element__like');
+
+    likeImage.addEventListener('click' , handleLikeImages)
+
+    const deleteCard = newInitialItem.querySelector('.element__delete')
+    deleteCard.addEventListener('click' , handleDeleteCard)
+
+    return newInitialItem;
+}
+
+const addElement = (initialItem) => {
+    elements.prepend(generateInititalItem(initialItem))
 };
 
-initialCards.forEach((initialCards) => {
-    addElement(initialCards);
+initialCards.forEach((initialItem) => {
+    addElement(initialItem);
 });
 
 // События
 formElement.addEventListener('submit', formSubmitHandler);
 openPopupButton.addEventListener('click', openPopupAdd);
 closePopupButton.addEventListener('click', closePopupRemove);
+
 formPopupAdd.addEventListener('submit', handlerSubmitAddSourceForm);
-// formPopupAdd.addEventListener('submit', handlerSubmitAddtitleForm)
-// для add
 openPopupAddButton.addEventListener('click',openPopupAddPhoto);
-closePopupAddButton.addEventListener('click',closePopupAddPhoto)
+closePopupAddButton.addEventListener('click',closePopupAddPhoto);
 
 
 
